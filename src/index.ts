a58c5b7e1beb12a59dc99480b9b246d70447f1dc
ShuchiKrishnani <shuchitakrishnani@gmail.com>
2020-06-04 22:47:29 +0530

    indentation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Server } from "./server";
- 
+
 const server = new Server();
- 
+
 server.listen(port => {
     console.log(`this si env = ${process.env.NODE_ENV} `)
- console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server is listening on http://localhost:${port}`);
 });

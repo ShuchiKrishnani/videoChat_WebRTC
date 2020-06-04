efff9a6602d1ab210ec8f4e89a1363d0f5f4fcef
ShuchiKrishnani <shuchitakrishnani@gmail.com>
2020-06-04 22:46:10 +0530

    added express dependency for production

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Server } from "./server";
 const server = new Server();
  
 server.listen(port => {
+    console.log(`this si env = ${process.env.NODE_ENV} `)
  console.log(`Server is listening on http://localhost:${port}`);
 });
